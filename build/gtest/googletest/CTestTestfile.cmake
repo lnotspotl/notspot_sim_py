4de9fd24024dfd643251414482e949384a38b0c2
lnotspotl <lnotspotl@gmail.com>
2021-05-07 16:50:54 +0200

    Hello

---
+++ build/gtest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest/googletest
+# Build directory: /home/kuba/github_projects/notspot_sim/build/gtest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
